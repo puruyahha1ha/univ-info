233db50c573b343196360f4d53e23600cb74a64d
puruyahha1ha <puruyahha1ha@gmail.com>
2025-03-16 10:20:28 +0900

    homeの作成。

---
+++ app/Livewire/Home/FeaturesSection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class FeaturesSection extends Component
+{
+    public array $features = [
+        [
+            'icon' => 'book',
+            'color' => 'indigo',
+            'title' => '豊富な入試情報',
+            'description' => '最新の入試情報を随時更新。大学の詳細情報や受験科目などを簡単に確認できます。学習計画の立案に役立ちます。'
+        ],
+        [
+            'icon' => 'document-text',
+            'color' => 'purple',
+            'title' => '個別学習プラン',
+            'description' => '一人ひとりの目標や弱点に合わせた学習プランを自動提案。効率的に勉強を進められます。'
+        ],
+        [
+            'icon' => 'clipboard-check',
+            'color' => 'yellow',
+            'title' => 'カスタムテスト機能',
+            'description' => '過去問や模試を自由に組み合わせ、あなた専用の模擬試験を作成。弱点克服に役立ちます。'
+        ]
+    ];
+
+    public function render()
+    {
+        return view('livewire.home.features-section');
+    }
+}
